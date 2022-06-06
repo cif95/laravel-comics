239c9e92568d6f927b7cf88fcca0a01b0f46a525
cif95 <vivianacifarelli9@gmail.com>
2022-06-06 13:21:50 +0200

    slide function correction

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,12 @@ let items = document.querySelectorAll('img.slider-item');
 let activeItem = 0;
 
 document.getElementById('back-arrow').addEventListener('click', function() {
-	slide(items, 'active', 'back' );
+	slide(items, 'active', false );
 });
 
 
 document.getElementById('next-arrow').addEventListener('click', function() {
-	slide(items, 'active', 'next' );
+	slide(items, 'active', true );
 });
 
 
@@ -17,18 +17,18 @@ document.getElementById('next-arrow').addEventListener('click', function() {
  * function that slides a carousel
  * @param {*} sliderItemList carousel items array
  * @param {*} className css class to be added on the current active item and removed on the previous active item
- * @param {*} direction sliding direction -> must be 'next' or 'back'
+ * @param {*} isDirectionNext sliding direction: boolean value, if true slides next, if false slides back
  */
-function slide( sliderItemList, className, direction ) {
+function slide( sliderItemList, className, isDirectionNext ) {
 	sliderItemList[activeItem].classList.remove(className);
-	if ( direction == 'next') {
+	if ( isDirectionNext === true) {
 		if ( activeItem === sliderItemList.length - 1 ){
 			activeItem = 0;
 		} else {
 			activeItem++;
 		}
 	}
-	if ( direction == 'back') {
+	if ( isDirectionNext === false ) {
 		if ( activeItem === 0 ){
 			activeItem = items.length - 1;
 		} else {
